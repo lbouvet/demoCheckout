72cc2265f0bc3cc1f0e1315fdd873e00684ac7b8
lbouvet <lbouvet@campus-eni.fr>
2021-06-29 12:30:44 +0200

    AJOUT Contact servlet

---
+++ src/main/java/com/example/demoGitCheckout/ContactServlet.java
@@ -0,0 +1,20 @@
+package com.example.demoGitCheckout;
+
+import javax.servlet.*;
+import javax.servlet.http.*;
+import javax.servlet.annotation.*;
+import java.io.IOException;
+
+@WebServlet(name = "ContactServlet", value = "/contact")
+public class ContactServlet extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        RequestDispatcher rd = request.getRequestDispatcher("WEB-INF/contact.jsp");
+        rd.forward(request, response);
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+    }
+}
--- src/main/webapp/WEB-INF/contact.jsp
+++ src/main/webapp/WEB-INF/contact.jsp
@@ -2,13 +2,13 @@
   Created by IntelliJ IDEA.
   User: lbouvet
   Date: 29/06/2021
-  Time: 12:20
+  Time: 12:24
   To change this template use File | Settings | File Templates.
 --%>
 <%@ page contentType="text/html;charset=UTF-8" language="java" %>
 <html>
 <head>
-    <title>Accueil</title>
+    <title>Contact</title>
 </head>
 <body>
     <header>
@@ -20,8 +20,7 @@
         </nav>
     </header>
     <main>
-        <h1>Accueil</h1>
-        <p>Bonjour ${name}</p>
+        <h1>Contact</h1>
     </main>
 </body>
 </html>
