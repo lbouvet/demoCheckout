b6c7926c2c74f3b370be6efd1c6555acf6ea25d6
lbouvet <lbouvet@campus-eni.fr>
2021-06-29 14:28:39 +0200

    Update Constact

--- src/main/java/com/example/demoGitCheckout/ContactServlet.java
+++ src/main/java/com/example/demoGitCheckout/ContactServlet.java
@@ -9,6 +9,11 @@ import java.io.IOException;
 public class ContactServlet extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+        String to = "mail@mail.org";
+
+        request.setAttribute("to", to);
+
         //commentaire
         RequestDispatcher rd = request.getRequestDispatcher("WEB-INF/contact.jsp");
         rd.forward(request, response);
